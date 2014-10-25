8374db2df306f4b5f0016a2d5ef1e58fb2112f25
Sam Philip <seivadmas@gmail.com>
2014-10-25 11:41:13 -0300

    Implemented language primitives

---
+++ README.md
@@ -0,0 +1,19 @@
+SCHEME
+======
+
+This small project is an implementation of Scheme in JavaScript.
+
+The language primitives are entirely defined in `scheme.js`.
+
+Each chapter corresponds to a chapter in the excellent book "The Little Schemer"
+by Friedman and Felleisen.
+
+The goal of these exercises is to learn to think recursively, and become more
+familiar with JavaScript syntax at the same time.
+
+Currently there are no JavaScript interpreters that support tail-call optimisation
+but this IS a part of EcmaScript 6.
+
+SCHEME relies heavily on (indeed it is pretty much defined by) recursion and
+recursive functions. For this reason it is recommended that this code be executed
+in an ES6-compatible interpreter if possible.
--- scheme.js
+++ scheme.js
@@ -1,2 +1,66 @@
 // The Little Schemer - JavaScript implementation
 // - By Sam Davies
+
+// SCHEME PRIMITIVE DEFINITIONS //
+//
+// Lists are implemented using JavaScript Array objects.
+// Atoms are either a String or a Number.
+//
+// boolean? functions with question marks are invalid JS, so where these are
+// referred to in the text they are replaced with their isBoolean equivalent.
+//
+// E.g. null? becomes isNull.
+
+"use strict";
+
+GLOBAL.car = function(list) {
+  if (!(list instanceof Array) || list.length === 0) {
+    throw 'car is only defined for non-empty lists.';
+  }
+  return list[0];
+};
+
+GLOBAL.cdr = function(list) {
+  if (!(list instanceof Array) || list.length === 0) {
+    throw 'cdr is only defined for non-empty lists.';
+  }
+  return list.slice(1);
+};
+
+GLOBAL.cons = function(sexp, list) {
+  if (!(list instanceof Array)) {
+    throw 'The second argument to cons must be a list.';
+  }
+  list.unshift(sexp);
+  return list;
+};
+
+GLOBAL.quote = function() {
+  return new Array;
+};
+
+GLOBAL.isNull = function(list) {
+  if (!(list instanceof Array)) {
+    throw 'isNull is defined only for lists.';
+  }
+  return (list.length === 0);
+};
+
+GLOBAL.isAtom = function(sexp) {
+  if (sexp instanceof Array) {
+    return false;
+  } else if (typeof sexp === 'number') {
+    return true;
+  } else if (typeof sexp === 'string') {
+    return true;
+  } else {
+    throw 'isAtom is only defined for valid S-expressions.';
+  }
+};
+
+GLOBAL.isEq = function(a, b) {
+  if (typeof a !== 'string' || typeof b !== 'string') {
+    throw 'isEq is only defined for comparison between non-numeric atoms.';
+  }
+  return (a === b);
+}
